159a032496889a3d0a155a5baa4878051a745193
Joe-Yang1 <348993072@tdsb.ca>
2026-04-15 10:46:50 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,37 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static String getFirstName(String firstName){
+		firstName = firstName.trim();
+		int space = firstName.indexOf(" ");
+		return firstName.substring(0, space);
+		}
+
+
+	public static String getLastName(String lastName){
+		lastName = lastName.trim();
+		int space = lastName.lastIndexOf(" ");
+		return lastName.substring(space + 1);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static boolean isValidName(String name){
+		int space = name.indexOf(" ");
+		int anotherSpace = name.lastIndexOf(" ");
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+	
+		if (space == -1 || space != anotherSpace){return false;}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		String beforeSpace = name.substring(0, space);
+		String afterSpace = name.substring(space + 1);
+		
+		if (beforeSpace.length() < 2 || afterSpace.length()<2){
+			return false;
+		}
+		return true;
 
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	
+			
 }
+}
+
+
